3fc1c4b14446fb1e38d244bc9d71e5e2645f46f5
Richard Aiello <richaiello17@gmail.com>
2022-07-20 18:15:23 -0400

    Styles for Header.jsx and Hero.jsx on tablet.

--- src/components/hero/HeroImage.jsx
+++ src/components/hero/HeroImage.jsx
@@ -12,7 +12,8 @@ const HeroImage = props => {
       <img
         src="./hero-mobile.webp"
         alt="29,000 members and 1,451 course hours."
-        className="hero__image mx-auto"
+        className="w-[min(100%,400px)] mx-auto object-cover 
+        md:absolute md:w-full md:left-[360px] md:top-[-260px]"
         loading="lazy"
       />
     </picture>
