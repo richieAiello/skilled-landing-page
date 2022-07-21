8ddd7aa9a43e36bf06105b46d14cf63f061867d6
Richard Aiello <richaiello17@gmail.com>
2022-07-21 08:21:51 -0400

    Fixed the placement of link in CourseCard.jsx.

--- src/components/course/CourseCard.jsx
+++ src/components/course/CourseCard.jsx
@@ -1,8 +1,8 @@
 const CourseCard = props => {
   return (
     <div
-      className="relative max-w-md min-h-[260px] bg-white rounded-[10px] pt-14 pb-8 px-7 mx-auto
-     md:max-w-full md:m-[unset] lg:h-[322px] lg:pt-16 "
+      className="relative flex flex-col max-w-md min-h-[260px] bg-white rounded-[10px] pt-14 pb-8 px-7 mx-auto
+     md:max-w-full md:m-[unset] lg:h-[322px] lg:pt-16 lg:pb-10"
     >
       <h3
         className="text-blue font-extrabold text-[1.25rem] leading-[1.575rem] capitalize mb-4
@@ -13,7 +13,7 @@ const CourseCard = props => {
       <p className="text-grey mb-6">{props.text}</p>
       <a
         href="#"
-        className="text-hot-pink text-lg font-bold 
+        className="text-hot-pink text-lg font-bold mt-auto
         capitalize duration-300 hover:text-pink focus:text-pink"
       >
         Get Started
